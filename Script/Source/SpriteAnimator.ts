665451d559feb0e8e89f495f43d2c4cfb69f3ce3
Plagiatus <lukas.scheuerle@web.de>
2024-06-03 09:17:56 +0200

    fix animation flicker

--- Script/Source/SpriteAnimator.ts
+++ Script/Source/SpriteAnimator.ts
@@ -14,8 +14,8 @@ namespace Script {
             this.mtx = _mtx;
             this.sprite = _as;
             this.startTime = _startTime;
-            this.totalTime = Math.floor((_as.frames / _as.fps) * 1000);
-            this.frameTime = Math.floor((1 / _as.fps) * 1000);
+            this.totalTime = (_as.frames / _as.fps) * 1000;
+            this.frameTime = (1 / _as.fps) * 1000;
 
             this.frameWidth = _as.width / _as.totalWidth;
             this.frameHeight = _as.height / _as.totalHeight;
